470efd7aa131d4878f7d6eb97536a74a6cdc07c3
eymeric <eymericdechelette@gmail.com>
2024-04-21 18:46:39 +0200

    migrate to flake-parts and enable nixfmt and precommit

--- flake.nix
+++ flake.nix
@@ -10,7 +10,15 @@
     # Official NixOS package source, using nixos-unstable branch here
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
-    nur.url = github:nix-community/NUR;
+
+    systems.url = "github:nix-systems/default";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    nur.url = "github:nix-community/NUR";
 
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.3.0";
@@ -39,95 +47,109 @@
 
     treefmt-nix.url = "github:numtide/treefmt-nix";
     pre-commit-hooks-nix = {
-      url = github:cachix/pre-commit-hooks.nix;
+      url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    self,
-    nixpkgs,
-    systems,
-    nixpkgs-unstable,
-    home-manager,
-    flatpaks,
-    plasma-manager,
-    nur,
-    lanzaboote,
-    agenix,
-    treefmt-nix,
-    pre-commit-hooks-nix,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-    pkgs-unstable = import nixpkgs-unstable {
-      inherit system;
-      config = {
-        cudaSupport = true;
-        allowUnfree = true;
-      };
-    };
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        cudaSupport = true;
-        allowUnfree = true;
-      };
-      overlays = [((import overlays/overlay.nix) pkgs-unstable)];
-    };
-    # Small tool to iterate over each systems
-    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = with inputs; [
+        pre-commit-hooks-nix.flakeModule
+        treefmt-nix.flakeModule
+      ];
 
-    # Eval the treefmt modules from ./treefmt.nix
-    treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
-  in {
-    nixosConfigurations = {
-      nixos-eymeric = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs agenix;}; # pretty hacky to pass agenix and inputs to configuration.nix
-        inherit system pkgs;
-        modules = with inputs; [
-          ./system/configuration.nix
-          ./cachix.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
+      flake = {
+        nixosConfigurations = {
+          nixos-eymeric = with inputs; let
+            system = "x86_64-linux";
+            pkgs-unstable = import nixpkgs-unstable {
+              inherit system;
+              config = {
+                cudaSupport = true;
+                allowUnfree = true;
+              };
             };
-            home-manager.users.eymeric = import ./home.nix;
-          }
-          lanzaboote.nixosModules.lanzaboote
-          agenix.nixosModules.default
-        ];
+            pkgs = import nixpkgs {
+              inherit system;
+              config = {
+                cudaSupport = true;
+                allowUnfree = true;
+              };
+              overlays = [((import overlays/overlay.nix) pkgs-unstable)];
+            };
+          in
+            nixpkgs.lib.nixosSystem {
+              specialArgs = {
+                inherit inputs agenix;
+              };
+              inherit system pkgs;
+              modules = [
+                ./system/configuration.nix
+                ./cachix.nix
+                home-manager.nixosModules.home-manager
+                {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.extraSpecialArgs = {
+                    inherit inputs;
+                  };
+                  home-manager.users.eymeric = import ./home.nix;
+                }
+                lanzaboote.nixosModules.lanzaboote
+                agenix.nixosModules.default
+              ];
+            };
+        };
       };
-    };
-    # for `nix fmt`
-    formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
-    # for `nix flake check`
-    checks = eachSystem (pkgs: {
-      formatting = treefmtEval.${pkgs.system}.config.build.check self;
-    });
 
-    pre-commit = {
-      check.enable = true;
-      settings.hooks = {
-        editorconfig-checker = {
-          enable = true;
-          excludes = [
-            ".*\\.age"
-            ".*\\.gpg"
-          ];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        # Auto formatters. This also adds a flake check to ensure that the
+        # source tree was auto formatted.
+        treefmt.config = {
+          projectRootFile = ".git/config";
+          package = pkgs.treefmt;
+          flakeCheck = false; # use pre-commit's check instead
+          programs = {
+            alejandra.enable = true; # nix
+            shellcheck.enable = true;
+            shfmt = {
+              enable = true;
+              indent_size = null;
+            };
+            prettier.enable = true;
+          };
         };
-        ripsecrets = {
-          enable = true;
-          excludes = [".*\\.crypt"];
+
+        pre-commit = {
+          check.enable = true;
+          settings.hooks = {
+            editorconfig-checker = {
+              enable = true;
+              excludes = [
+                ".*\\.age"
+              ];
+            };
+            ripsecrets = {
+              enable = true;
+            };
+            treefmt.enable = true;
+            typos = {
+              enable = true;
+            };
+          };
         };
-        treefmt.enable = true;
-        typos = {
-          enable = true;
-          excludes = [".*\\.crypt"];
+        devShells.default = pkgs.mkShell {
+          # Inherit all of the pre-commit hooks.
+          inputsFrom = [config.pre-commit.devShell];
         };
       };
     };
-  };
 }
