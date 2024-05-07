9149d769491bda3b2ca1ccecc0141343e77b81f4
eymeric <eymericdechelette@gmail.com>
2024-05-08 00:18:53 +0200

    a little bit better

--- common.nix
+++ common.nix
@@ -10,38 +10,24 @@
 in {
   imports = [
     (lib.mkAliasOptionModule ["hm"] ["home-manager" "users" username])
+    (import ./home.nix
+      {
+        inherit
+          pkgs
+          config
+          lib
+          inputs
+          stateVersion
+          username
+          ;
+      })
     ./cachix.nix
     ./wifi.nix
     ./apps/vm.nix
-    apps/zsh.nix
-    apps/keepassxc.nix
-    apps/plasma/plasma.nix
-    apps/neovim.nix
-    apps/onedrive/onedrive.nix
-    apps/ghostwriter.nix
-    apps/thunderbird.nix
-    apps/vscode.nix
-    apps/vesktop/vesktop.nix
-    #apps/espanso.nix
-    apps/konsole.nix
-    apps/homebank.nix
   ];
 
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
   };
-
-  hm =
-    import ./home.nix
-    {
-      inherit
-        pkgs
-        config
-        lib
-        inputs
-        stateVersion
-        username
-        ;
-    };
 }
