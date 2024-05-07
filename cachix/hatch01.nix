a1a1669d53d579e57f7f6ca5686b6dc673621f9d
eymeric <eymericdechelette@gmail.com>
2024-05-07 11:40:14 +0200

    try enable hatch01 cache

---
+++ cachix/hatch01.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://hatch01.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hatch01.cachix.org-1:MiLD2xTBHcs0zIYozmA//rR+/svETz0AXzDFmI2Wjso="
+      ];
+    };
+  };
+}
