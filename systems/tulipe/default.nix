89661a06656cac1f43c92542cb9bf66533231ae6
eymeric <eymericdechelette@gmail.com>
2024-06-06 20:18:44 +0200

    use cloudflare dns

--- systems/tulipe/default.nix
+++ systems/tulipe/default.nix
@@ -51,8 +51,10 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
-
+  networking = {
+    networkmanager.enable = true;
+    nameservers = ["1.1.1.1"];
+  };
   # Configure console keymap
   console.keyMap = "fr";
 
