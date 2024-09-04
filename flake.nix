d44b213494ac45b4651dc0da6ac6113267472146
eymeric <eymericdechelette@gmail.com>
2024-09-04 19:49:43 +0200

    add cache

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   nixConfig = {
     experimental-features = ["nix-command" "flakes"];
     extra-trusted-substituters = [
+      "https://cache.onyx.ovh"
       "https://cache.garnix.io"
       "https://nix-community.cachix.org"
       "https://cuda-maintainers.cachix.org"
@@ -12,6 +13,7 @@
       "https://nixpkgs-unfree.cachix.org"
     ];
     extra-trusted-public-keys = [
+
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
