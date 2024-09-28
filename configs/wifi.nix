7c5c7c9d2d925bc3c5e399e7413b035d23e82fcd
eymeric <eymericdechelette@gmail.com>
2024-09-28 11:07:44 +0200

    Refactor wifi.nix to remove unused code and update profiles
    This commit refactors the wifi.nix file by removing unused code and updating the profiles. The `age.identityPaths` configuration is removed, and the `networking.networkmanager.ensureProfiles.profiles` section is updated to use a `mkWifi` function to generate the profiles. The `HHCuisine` profile is updated to include the wifi connection details and security settings. The `eduroam` profile is also added with the necessary configuration for connecting to the eduroam network.
    
    Refactor wifi.nix to remove unused code and update profiles

--- configs/wifi.nix
+++ configs/wifi.nix
@@ -10,7 +10,6 @@
   };
 in {
   age = {
-    identityPaths = ["/etc/age/key"];
     # secrets = mkSecrets {
     #   "desktop/wifi" = {root = true;};
     # };
@@ -18,81 +17,59 @@ in {
   };
   #networks
   networking.networkmanager.ensureProfiles = {
-    profiles = {
-      HHCuisine = {
-        connection = {
-          id = "HHCuisine";
-          uuid = "4da44d32-bd84-4e91-9f7b-649567c0bced";
-          type = "wifi";
-          permissions = "";
     environmentFiles = ["${config.age.secrets."desktop/wifi".path}"];
+    profiles = let
+      mkWifi = name: args: {
+        name = {
+          connection = {
+            id = name;
+            # uuid = "4da44d32-bd84-4e91-9f7b-649567c0bced";
+            type = "wifi";
+            permissions = "";
+          };
+          wifi = {
+            mode = "infrastructure";
+            ssid = name;
+          };
+          wifi-security = {
+            key-mgmt = "wpa-psk";
+            psk =
+              if (args.password or "") != ""
+              then args.password
+              else "$${name}_password";
+          };
+          inherit ipv4 ipv6;
         };
-        wifi = {
-          mode = "infrastructure";
-          ssid = "HHCuisine";
-        };
-        wifi-security = {
-          key-mgmt = "wpa-psk";
-          psk = "$HHCuisine_password";
-        };
-        inherit ipv4 ipv6;
       };
-      HHCuisine5G = {
-        connection = {
-          id = "HHCuisine5G";
-          uuid = "af23e6ba-5e1d-490c-898f-0d194055107e";
-          type = "wifi";
-          permissions = "";
+    in
+      mkWifi "HHCuisine" {}
+      // mkWifi "HHCuisine5G" {password = "$HHCuisine_password";}
+      // mkWifi "Onyx" {}
+      // mkWifi "Onyx 5G" {password = "$Onyx_password";}
+      // mkWifi "Installe onyx !" {password = "$onyx2_password";}
+      // {
+        eduroam = {
+          connection = {
+            id = "eduroam";
+            type = "wifi";
+            permissions = "";
+          };
+          wifi = {
+            mode = "infrastructure";
+            ssid = "eduroam";
+          };
+          wifi-security = {
+            key-mgmt = "wpa-eap";
+          };
+          "802-1x" = {
+            eap = "peap";
+            identity = "$eduroam_user";
+            password-flags = "1";
+            password = "$eduroam_password";
+            phase2-auth = "mschapv2";
+          };
+          inherit ipv4 ipv6;
         };
-        wifi = {
-          mode = "infrastructure";
-          ssid = "HHCuisine5G";
-        };
-        wifi-security = {
-          key-mgmt = "wpa-psk";
-          psk = "$HHCuisine_password";
-        };
-        inherit ipv4 ipv6;
       };
-      "Installe onyx !" = {
-        connection = {
-          id = "Installe onyx !";
-          uuid = "30112829-d494-4582-beb9-caa22bfc02ca";
-          type = "wifi";
-          permissions = "";
-        };
-        wifi = {
-          mode = "infrastructure";
-          ssid = "Installe onyx !";
-        };
-        wifi-security = {
-          key-mgmt = "wpa-psk";
-          psk = "$onyx_password";
-        };
-        inherit ipv4 ipv6;
-      };
-      eduroam = {
-        connection = {
-          id = "eduroam";
-          type = "wifi";
-          permissions = "";
-        };
-        wifi = {
-          mode = "infrastructure";
-          ssid = "eduroam";
-        };
-        wifi-security = {
-          key-mgmt = "wpa-eap";
-        };
-        "802-1x" = {
-          eap = "peap";
-          identity = "$eduroam_user";
-          password-flags = "1";
-          password = "$eduroam_password";
-          phase2-auth = "mschapv2";
-        };
-        inherit ipv4 ipv6;
-      };
-    };
   };
 }
