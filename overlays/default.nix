9b9e9c2e009bbed28b10c3322d9b89963f482a33
eymeric <eymericdechelette@gmail.com>
2024-10-02 11:17:41 +0200

    Refactor default.nix to add mautrix-signal package
    This commit refactors the default.nix file to add the mautrix-signal package. The version is set to "0.7.1" and the source is fetched from GitHub using the specified revision and hash. The vendorHash is also updated.

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,4 +12,16 @@
         value = unstable.${x};
       })
       unstablePackages)
+    // {
+      mautrix-signal = prev.mautrix-signal.overrideAttrs rec {
+        version = "0.7.1";
+        src = prev.fetchFromGitHub {
+          owner = "mautrix";
+          repo = "signal";
+          rev = "v${version}";
+          hash = "sha256-OjWRdYAxjYMGZswwKqGKUwCIc5qHkNBTQgIcbiRquH0=";
+        };
+        vendorHash = "sha256-oV8ILDEyMpOZy5m2mnPAZj5XAhleO8yNz49wxvZboVs=";
+      };
+    }
 )
