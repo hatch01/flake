0fadec6142e50efe6467749c33add837bcce54fc
eymeric <eymericdechelette@gmail.com>
2024-11-07 09:07:04 +0100

    Implement background update process for GitLab configuration updater

--- apps/gitlab/config_updater.nix
+++ apps/gitlab/config_updater.nix
@@ -5,6 +5,36 @@
   ...
 }: let
   inherit (lib) mkEnableOption mkIf mkOption types;
+  script = ''
+    read request
+    path=$(echo "$request" | awk '{print $2}' | cut -d'?' -f1)
+
+    case "$path" in
+      /update)
+        # Send immediate response to avoid timeout
+        echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nUpdating repository..."
+
+        # Run the update process in the background
+        (
+          ssh-keyscan forge.onyx.ovh >> ~/.ssh/known_hosts
+          cd /tmp/
+          if [ ! -d "flake/.git" ]; then
+              git clone "gitlab@forge.onyx.ovh:eymeric/flake.git" "flake"
+          fi
+
+          cd "flake" || exit
+          git reset --hard HEAD
+          git pull
+          nix flake update --commit-lock-file --accept-flake-config
+          git push
+        ) &  # Run in the background
+        ;;
+      *)
+        # Default response for unknown paths
+        echo -e "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nPath not found"
+        ;;
+    esac
+  '';
 in {
   options = {
     gitlab = {
@@ -36,36 +66,7 @@ in {
       };
       script = ''
         socat TCP4-LISTEN:${builtins.toString config.gitlab.configUpdater.port},reuseaddr,fork SYSTEM:${
-          lib.getExe (pkgs.writeShellScriptBin "route-hander" ''
-            read request
-            path=$(echo "$request" | awk '{print $2}' | cut -d'?' -f1)
-
-            case "$path" in
-              /update)
-                # Send immediate response to avoid timeout
-                echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nUpdating repository..."
-
-                # Run the update process in the background
-                (
-                  ssh-keyscan forge.onyx.ovh >> ~/.ssh/known_hosts
-                  cd /tmp/
-                  if [ ! -d "flake/.git" ]; then
-                      git clone "gitlab@forge.onyx.ovh:eymeric/flake.git" "flake"
-                  fi
-
-                  cd "flake" || exit
-                  git reset --hard HEAD
-                  git pull
-                  nix flake update --commit-lock-file --accept-flake-config
-                  git push
-                ) &  # Run in the background
-                ;;
-              *)
-                # Default response for unknown paths
-                echo -e "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nPath not found"
-                ;;
-            esac
-          '')
+          lib.getExe (pkgs.writeShellScriptBin "route-hander" script)
         }
       '';
     };
