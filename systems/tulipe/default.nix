4dabbcabd0f271cd70017bb92d736796d41c9ca7
eymeric <eymericdechelette@gmail.com>
2024-11-13 09:52:55 +0100

    Update boot kernel packages to use the latest version

--- systems/tulipe/default.nix
+++ systems/tulipe/default.nix
@@ -35,7 +35,7 @@
   };
 
   # Bootloader.
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.supportedFilesystems = ["ntfs"];
