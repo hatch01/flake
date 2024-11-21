1225a377c6bccac23a78d6e7c35761218994831f
eymeric <eymericdechelette@gmail.com>
2024-11-21 19:59:58 +0100

    Enhance platformio.nix to conditionally include arduino-ide for x86_64-linux systems

--- apps/dev/platformio.nix
+++ apps/dev/platformio.nix
@@ -4,20 +4,21 @@
   config,
   ...
 }: let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkEnableOption mkIf optionals;
 in {
   options = {
     platformio.enable = mkEnableOption "platformio";
   };
 
   config = mkIf config.platformio.enable {
-    environment.systemPackages = with pkgs; [
-      platformio
-      platformio-core
-      avrdude
-      arduino-ide
-      (python3.withPackages (ps: with python3Packages; [pyserial]))
-    ];
+    environment.systemPackages = with pkgs;
+      [
+        platformio
+        platformio-core
+        avrdude
+        (python3.withPackages (ps: with python3Packages; [pyserial]))
+      ]
+      ++ optionals (pkgs.system == "x86_64-linux") [pkgs.arduino-ide];
     services.udev.packages = [
       pkgs.platformio-core
       pkgs.openocd
