7d6cba246aa113ba1445c9afb1b59bfeed3b3428
eymeric <eymericdechelette@gmail.com>
2024-12-01 19:20:08 +0100

    Remove systemd-boot enablement from bootloader configuration

--- systems/tulipe/default.nix
+++ systems/tulipe/default.nix
@@ -36,8 +36,8 @@
 
   # Bootloader.
   boot.kernelPackages = pkgs.linuxPackages_6_11;
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.supportedFilesystems = ["ntfs"];
   boot.binfmt.emulatedSystems = ["aarch64-linux"];
   boot.kernelModules = ["v4l2loopback"];
