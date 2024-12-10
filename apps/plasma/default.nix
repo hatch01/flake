9de37b07444b35d511aea55a1cdb0ed7e03b9112
eymeric <eymericdechelette@gmail.com>
2024-12-10 14:36:48 +0100

    Update excluded packages in plasma configuration to use kdePackages

--- apps/plasma/default.nix
+++ apps/plasma/default.nix
@@ -34,7 +34,7 @@ in {
       desktopManager.plasma6.enable = true;
     };
 
-    environment.plasma6.excludePackages = with pkgs.libsForQt5; [
+    environment.plasma6.excludePackages = with pkgs.kdePackages; [
       elisa
       kwrite
     ];
