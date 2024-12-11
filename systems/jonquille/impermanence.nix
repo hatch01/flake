bd3702aadd7144ad448a8be680aecb5a45b348ad
eymeric <eymericdechelette@gmail.com>
2024-12-11 18:45:12 +0100

    Refactor subvolume cleanup logic to improve efficiency and simplify old subvolume deletion

--- systems/jonquille/impermanence.nix
+++ systems/jonquille/impermanence.nix
@@ -8,34 +8,26 @@ in {
   boot.initrd.postDeviceCommands = lib.mkAfter ''
     mkdir /btrfs_tmp
     mount /dev/disk/by-partlabel/disk-main-root /btrfs_tmp
-
-    if [ -e /btrfs_tmp/rootfs ]; then
+    if [[ -e /btrfs_tmp/rootfs ]]; then
         mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date -d "@$(stat -c %Y /btrfs_tmp/rootfs)" "+%Y-%m-%-d_%H:%M:%S")
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/rootfs)" "+%Y-%m-%-d_%H:%M:%S")
         mv /btrfs_tmp/rootfs "/btrfs_tmp/old_roots/$timestamp"
     fi
 
     delete_subvolume_recursively() {
-        IFS="\n"
+        IFS=$'\n'
         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "$1/$i"
+            delete_subvolume_recursively "/btrfs_tmp/$i"
         done
         btrfs subvolume delete "$1"
     }
 
-    num_subvolumes_to_keep=3
-    old_subvolumes=$(ls -dt /btrfs_tmp/old_roots/* 2>/dev/null)
-    count=0
-    for subvolume in $old_subvolumes; do
-        count=$((count + 1))
-        if [ $count -gt $num_subvolumes_to_keep ]; then
-            delete_subvolume_recursively "$subvolume"
-        fi
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +5); do
+        delete_subvolume_recursively "$i"
     done
 
     btrfs subvolume create /btrfs_tmp/rootfs
     umount /btrfs_tmp
-
   '';
 
   environment.persistence."/persistent" = {
