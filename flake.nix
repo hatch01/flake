0ced66b89e556ead3e05ee939b110d835856bce0
eymeric <eymericdechelette@gmail.com>
2024-12-14 15:23:59 +0100

    Update flatpak references to version 0.5.2 in flake.lock and flake.nix

--- flake.nix
+++ flake.nix
@@ -70,7 +70,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flatpaks.url = "github:gmodena/nix-flatpak/?ref=v0.5.1";
+    flatpaks.url = "github:gmodena/nix-flatpak/?ref=v0.5.2";
 
     plasma-manager.url = "github:pjones/plasma-manager";
     plasma-manager.inputs.nixpkgs.follows = "nixpkgs";
