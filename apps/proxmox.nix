1b675c2c4b5b1dd2b015b75809177f49dccdffad
eymeric <eymericdechelette@gmail.com>
2024-12-17 08:46:14 +0100

    proxmox.nix: Set default IP address for Proxmox VE

--- apps/proxmox.nix
+++ apps/proxmox.nix
@@ -27,6 +27,7 @@ in {
     # enable proxmox
     services.proxmox-ve = {
       enable = true;
+      ipAddress = "192.168.0.1";
     };
     nixpkgs.overlays = [
       inputs.proxmox-nixos.overlays.${system}
