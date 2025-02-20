dff6f8255a6dd1f1d2f155f56e859979a10b0acd
eymeric <eymericdechelette@gmail.com>
2025-02-20 19:17:33 +0100

    refactor: clean up flake.nix by removing unused substituters and keys

--- flake.nix
+++ flake.nix
@@ -6,26 +6,14 @@
     substituters = [
       "https://cache.onyx.ovh"
       "https://cache.nixos.org"
-      "https://cache.garnix.io"
-      "https://nix-community.cachix.org"
       "https://cuda-maintainers.cachix.org"
-      "https://hatch01.cachix.org"
-      "https://numtide.cachix.org"
-      "https://nixpkgs-unfree.cachix.org"
       "https://cache.saumon.network/proxmox-nixos"
-      "https://ghostty.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cache.onyx.ovh:2wUG6wsx5slbKUgkHT6GJuQ5k2StuUc8ysZQ2W+fbxA="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "hatch01.cachix.org-1:MiLD2xTBHcs0zIYozmA//rR+/svETz0AXzDFmI2Wjso="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
       "proxmox-nixos:nveXDuVVhFDRFx8Dn19f1WDEaNRJjPrF2CPD2D+m1ys="
-      "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
     ];
 
     trusted-users = [
