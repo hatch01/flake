03498889c4c7e3d53991b66587f23640bf94fa8b
eymeric <eymericdechelette@gmail.com>
2025-04-20 11:17:30 +0200

    nique hyperhdr autostart

--- apps/plasma/autostart/hyperhdr.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Wait for dbus service to be available
-until dbus-send --print-reply --dest=org.freedesktop.DBus / org.freedesktop.DBus.ListNames; do
-    sleep 1
-done
-
-# Start HyperHDR
-hyperhdr
