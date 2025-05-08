e0c821503cb479178b683060571c85d7de4acc8f
eymeric <eymericdechelette@gmail.com>
2025-05-08 19:06:43 +0200

    Revert "Reapply "fix: update Home Assistant database host IP address in postgres configuration""
    This reverts commit 00f96af524aa336e498fab8d4e5c17e15a4ddc77.

--- apps/postgres.nix
+++ apps/postgres.nix
@@ -22,7 +22,7 @@ in {
         (lib.concatStrings (config.postgres.initialScripts or []));
       authentication =
         lib.mkIf config.homeassistant.enable
-        "host homeassistant_db     homeassistant   172.17.0.1/16      md5 # Home Assistant server";
+        "host homeassistant_db     homeassistant   192.168.1.200/24      md5 # Home Assistant server";
       enableTCPIP = config.homeassistant.enable;
     };
   };
