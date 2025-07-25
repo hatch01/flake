51eaefdce410ec3bbc512b08dd1fb5f95186c395
eymeric <eymericdechelette@gmail.com>
2025-07-25 09:29:23 +0200

    Reformat Nextcloud app and settings definitions for clarity

--- apps/nextcloud.nix
+++ apps/nextcloud.nix
@@ -78,94 +78,92 @@ in
         };
 
         # apps
-        extraApps =
-          {
-            inherit (config.services.nextcloud.package.packages.apps)
-              contacts
-              calendar
-              tasks
-              mail
-              cospend
-              end_to_end_encryption
-              # forms
-              groupfolders
-              # maps
-              music
-              notes
-              previewgenerator
-              deck
-              cookbook
-              ;
-          }
-          // optionals config.authelia.enable {
-            oidc_login = pkgs.fetchNextcloudApp {
-              license = "agpl3Plus";
-              url = "https://github.com/pulsejet/nextcloud-oidc-login/releases/download/v3.2.2/oidc_login.tar.gz";
-              sha256 = "sha256-RLYquOE83xquzv+s38bahOixQ+y4UI6OxP9HfO26faI=";
-            };
-          }
-          // optionals config.onlyofficeDocumentServer.enable {
-            inherit (config.services.nextcloud.package.packages.apps)
-              onlyoffice
-              ;
+        extraApps = {
+          inherit (config.services.nextcloud.package.packages.apps)
+            contacts
+            calendar
+            tasks
+            mail
+            cospend
+            end_to_end_encryption
+            # forms
+            groupfolders
+            # maps
+            music
+            notes
+            previewgenerator
+            deck
+            cookbook
+            ;
+        }
+        // optionals config.authelia.enable {
+          oidc_login = pkgs.fetchNextcloudApp {
+            license = "agpl3Plus";
+            url = "https://github.com/pulsejet/nextcloud-oidc-login/releases/download/v3.2.2/oidc_login.tar.gz";
+            sha256 = "sha256-RLYquOE83xquzv+s38bahOixQ+y4UI6OxP9HfO26faI=";
           };
+        }
+        // optionals config.onlyofficeDocumentServer.enable {
+          inherit (config.services.nextcloud.package.packages.apps)
+            onlyoffice
+            ;
+        };
 
         extraAppsEnable = true;
         # appstoreEnable = true; # DO NOT ENABLE, it will break the declarative config for apps
 
-        settings =
-          {
-            mail_from_address = "nextcloud";
-            mail_smtpmode = "smtp";
-            mail_sendmailmode = "smtp";
-            mail_domain = "onyx.ovh";
-            mail_smtphost = "mtp.free.fr";
-            mail_smtpauth = 1;
-            mail_smtpport = 587;
-            mail_smtpname = "eymeric.monitoring";
-            maintenance_window_start = 1;
-            default_phone_region = "FR";
-            log_type = "file";
-          }
-          // optionals config.authelia.enable {
-            user_oidc = {
-              single_logout = false;
-              auto_provision = true;
-              soft_auto_provision = true;
-            };
-
-            allow_user_to_change_display_name = false;
-            lost_password_link = "disabled";
-            oidc_login_provider_url = "https://${config.authelia.domain}";
-            oidc_login_client_id = "nextcloud";
-            # oidc_login_client_secret = "insecure_secret"; # set in secret file
-            oidc_login_auto_redirect = true;
-            oidc_login_end_session_redirect = false;
-            oidc_login_button_text = "Log in with Authelia";
-            oidc_login_hide_password_form = false;
-            oidc_login_use_id_token = false;
-            oidc_login_attributes = {
-              id = "preferred_username";
-              name = "name";
-              mail = "email";
-              groups = "groups";
-            };
-            oidc_login_default_group = "oidc";
-            oidc_login_use_external_storage = false;
-            oidc_login_scope = "openid profile email groups";
-            oidc_login_proxy_ldap = false;
-            oidc_login_disable_registration = false; # different from doc, to enable auto creation of new users
-            oidc_login_redir_fallback = false;
-            oidc_login_tls_verify = true;
-            oidc_create_groups = false;
-            oidc_login_webdav_enabled = false;
-            oidc_login_password_authentication = false;
-            oidc_login_public_key_caching_time = 86400;
-            oidc_login_min_time_between_jwks_requests = 10;
-            oidc_login_well_known_caching_time = 86400;
-            oidc_login_update_avatar = false;
-            oidc_login_code_challenge_method = "S256";
+        settings = {
+          mail_from_address = "nextcloud";
+          mail_smtpmode = "smtp";
+          mail_sendmailmode = "smtp";
+          mail_domain = "onyx.ovh";
+          mail_smtphost = "mtp.free.fr";
+          mail_smtpauth = 1;
+          mail_smtpport = 587;
+          mail_smtpname = "eymeric.monitoring";
+          maintenance_window_start = 1;
+          default_phone_region = "FR";
+          log_type = "file";
+        }
+        // optionals config.authelia.enable {
+          user_oidc = {
+            single_logout = false;
+            auto_provision = true;
+            soft_auto_provision = true;
           };
+
+          allow_user_to_change_display_name = false;
+          lost_password_link = "disabled";
+          oidc_login_provider_url = "https://${config.authelia.domain}";
+          oidc_login_client_id = "nextcloud";
+          # oidc_login_client_secret = "insecure_secret"; # set in secret file
+          oidc_login_auto_redirect = true;
+          oidc_login_end_session_redirect = false;
+          oidc_login_button_text = "Log in with Authelia";
+          oidc_login_hide_password_form = false;
+          oidc_login_use_id_token = false;
+          oidc_login_attributes = {
+            id = "preferred_username";
+            name = "name";
+            mail = "email";
+            groups = "groups";
+          };
+          oidc_login_default_group = "oidc";
+          oidc_login_use_external_storage = false;
+          oidc_login_scope = "openid profile email groups";
+          oidc_login_proxy_ldap = false;
+          oidc_login_disable_registration = false; # different from doc, to enable auto creation of new users
+          oidc_login_redir_fallback = false;
+          oidc_login_tls_verify = true;
+          oidc_create_groups = false;
+          oidc_login_webdav_enabled = false;
+          oidc_login_password_authentication = false;
+          oidc_login_public_key_caching_time = 86400;
+          oidc_login_min_time_between_jwks_requests = 10;
+          oidc_login_well_known_caching_time = 86400;
+          oidc_login_update_avatar = false;
+          oidc_login_code_challenge_method = "S256";
+        };
         # secret file currently only used to provide:
         # - oidc_login_client_secret for authelia
         # - mail_smtppassword for mail
