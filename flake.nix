61b1d21e647a05610bdf3c24feeb5030adb1c89c
eymeric <eymericdechelette@gmail.com>
2025-07-27 20:29:12 +0200

    Add aarch64-linux to supported systems

--- flake.nix
+++ flake.nix
@@ -118,7 +118,7 @@
   outputs =
     { flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+      systems = [ "x86_64-linux" "aarch64-linux" ];
       imports = [
         # inputs.pre-commit-hooks-nix.flakeModule
         # inputs.treefmt-nix.flakeModule
