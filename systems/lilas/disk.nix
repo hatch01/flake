bf1ea7b3884bad163e65058e27b365823bf3b8cb
eymeric <eymericdechelette@gmail.com>
2025-07-27 21:22:15 +0200

    Inline rootDisk variable in disk configuration

--- systems/lilas/disk.nix
+++ systems/lilas/disk.nix
@@ -1,66 +1,62 @@
 {
-  disko.devices =
-    let
-      rootDisk = "/dev/mmcblk1";
-    in
-    {
-      disk = {
-        main = {
-          device = rootDisk;
-          type = "disk";
-          content = {
-            type = "table";
-            format = "msdos";
-            partitions = {
-              PIBOOT = {
-                label = "PIBOOT";
-                type = "primary";
-                start = "0";
-                end = "256MiB";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                };
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/mmcblk1";
+        type = "disk";
+        content = {
+          type = "table";
+          format = "msdos";
+          partitions = {
+            PIBOOT = {
+              label = "PIBOOT";
+              type = "primary";
+              start = "0";
+              end = "256MiB";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
               };
-              PIPST = {
-                label = "PIPST";
-                type = "primary";
-                start = "256MiB";
-                end = "512MiB";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/var/lib/kvmd/pst";
-                  mountOptions = [ "reserved=0" ];
-                };
+            };
+            PIPST = {
+              label = "PIPST";
+              type = "primary";
+              start = "256MiB";
+              end = "512MiB";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/var/lib/kvmd/pst";
+                mountOptions = [ "reserved=0" ];
               };
-              PIROOT = {
-                label = "PIROOT";
-                type = "primary";
-                start = "512MiB";
-                end = "6656MiB";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                };
+            };
+            PIROOT = {
+              label = "PIROOT";
+              type = "primary";
+              start = "512MiB";
+              end = "6656MiB";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
-              PIMSD = {
-                label = "PIMSD";
-                type = "primary";
-                start = "6656MiB";
-                end = "100%";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/var/lib/kvmd/msd";
-                  mountOptions = [ "reserved=0" ];
-                };
+            };
+            PIMSD = {
+              label = "PIMSD";
+              type = "primary";
+              start = "6656MiB";
+              end = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/var/lib/kvmd/msd";
+                mountOptions = [ "reserved=0" ];
               };
             };
           };
         };
       };
     };
+  };
 }
