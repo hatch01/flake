240d54570553a3eed89abf17d64dba8e2798754c
eymeric <eymericdechelette@gmail.com>
2025-08-07 23:57:20 +0200

    Fix zsh.nix quoting for direnv and ZSH_PROFILE_STARTUP

--- apps/zsh.nix
+++ apps/zsh.nix
@@ -84,18 +84,18 @@ in
           webcam = lib.mkIf config.dev.androidtools.enable "${lib.getExe pkgs.scrcpy} --v4l2-sink=/dev/video0 --orientation=0";
         };
         initExtraFirst = ''
-          (( $${+commands[direnv]} )) && emulate zsh -c "$(direnv export zsh)"
+          (( ''${+commands[direnv]} )) && emulate zsh -c "$(direnv export zsh)"
           # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
           # Initialization code that may require console input (password prompts, [y/n]
           # confirmations, etc.) must go above this block; everything else may go below.
           if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
             source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
           fi
-          (( $${+commands[direnv]} )) && emulate zsh -c "$(direnv hook zsh)"
+          (( ''${+commands[direnv]} )) && emulate zsh -c "$(direnv hook zsh)"
         '';
         initContent = mkMerge [
           (mkBefore ''
-            if [ -n "$${ZSH_PROFILE_STARTUP:+x}" ]
+            if [ -n "''${ZSH_PROFILE_STARTUP:+x}" ]
             then
               zmodload zsh/zprof
             fi
