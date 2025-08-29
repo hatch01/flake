e754d888a89064f971ee5a9be058b54ef9e2ce22
eymeric <eymericdechelette@gmail.com>
2025-08-29 22:07:40 +0200

    feat: enable bolt for tulipe, dont know if its needed ^^

--- systems/tulipe/default.nix
+++ systems/tulipe/default.nix
@@ -73,6 +73,8 @@
     pkgs.utsushi
   ];
 
+  services.hardware.bolt.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
