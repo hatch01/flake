5b239f97671bd7cd666e13043f5d54683c4ec8eb
System administrator <root@jonquille.onyx.ovh>
2025-09-24 10:15:58 +0200

    fix: mautrix signal config migration

--- apps/matrix/signal.nix
+++ apps/matrix/signal.nix
@@ -21,8 +21,12 @@ in
       enable = true;
       registerToSynapse = true;
       settings = {
+        analytics = {
+          token = null;
+          url = "https://api.segment.io/v1/track";
+          user_id = null;
+        };
         bridge = {
-          displayname_template = "{{or .ContactName \"Unknown user\"}} (Signal)";
           permissions = {
             "*" = "relay";
             "${base_domain_name}" = "user";
@@ -33,15 +37,33 @@ in
           };
           sync_direct_chat_list = true;
         };
+        network = {
+          device_name = "mautrix-signal";
+          disappear_view_once = false;
+          displayname_template = "'{{or .ContactName \" Unknown user \"}} (Signal)'";
+          location_format = "'https://www.google.com/maps/place/%[1]s,%[2]s'";
+          note_to_self_avatar = "mxc://maunium.net/REBIVrqjZwmaWpssCZpBlmlL";
+          number_in_topic = true;
+          sync_contacts_on_startup = true;
+          use_contact_avatars = false;
+          use_outdated_profiles = false;
+        };
         homeserver = {
           address = "http://localhost:${toString config.matrix.port}";
           domain = base_domain_name;
         };
         appservice = {
-          database = {
-            type = "postgres";
-            uri = "postgresql:///mautrix-signal?host=/run/postgresql";
-          };
+          address = "https://localhost:29328";
+          async_transactions = false;
+          bot.avatar = "mxc://maunium.net/wPJgTQbZOtpBFmDNkiNEMDUp";
+        };
+        database = {
+          type = "postgres";
+          uri = "postgresql:///mautrix-signal?host=/run/postgresql";
+          max_conn_idle_time = null;
+          max_conn_lifetime = null;
+          max_idle_conns = 1;
+          max_open_conns = 5;
         };
       };
       environmentFile = config.age.secrets.matrix_shared_secret.path;
