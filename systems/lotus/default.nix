0f271a1ea62a403fb6575d74cfca8053757da921
eymeric <eymericdechelette@gmail.com>
2025-09-26 10:28:22 +0200

    feat: jira cli

--- systems/lotus/default.nix
+++ systems/lotus/default.nix
@@ -55,6 +55,10 @@
     };
   };
 
+  environment.systemPackages = with pkgs; [
+    jira-cli-go
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
