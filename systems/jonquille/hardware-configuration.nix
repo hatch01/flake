21eefa7e800bb20e61902d40853562378e6651a3
eymeric <eymericdechelette@gmail.com>
2025-10-11 12:23:25 +0200

    feat: cpu autofreq

--- systems/jonquille/hardware-configuration.nix
+++ systems/jonquille/hardware-configuration.nix
@@ -38,4 +38,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  services.auto-cpufreq.enable = true;
 }
