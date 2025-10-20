1a40a655ba4813a939bdf07fcfb8ddbdcf71eb18
eymeric <eymericdechelette@gmail.com>
2025-10-20 08:38:26 +0200

    fix: migrate git params

--- apps/git.nix
+++ apps/git.nix
@@ -24,36 +24,40 @@ in
       programs = {
         git = {
           enable = true;
-          userName = username;
-          userEmail = "eymericdechelette@gmail.com";
+          settings = {
+            user = {
+              email = "eymericdechelette@gmail.com";
+              name = username;
+            };
+            extraConfig = {
+              pull.rebase = true;
+              safe.directory = [
+                "/etc/nixos/.git"
+                "/etc/nixos"
+              ];
+              merge.conflictstyle = "zdiff3";
+              merge.tool = "vimdiff";
+              core.pager = "delta";
+              interactive.diffFilter = "delta --color-only";
+              delta = {
+                navigate = true;
+                dark = true;
+                side-by-side = true;
+              };
+              diff.colorMoved = "default";
+              rebase.autoStash = true;
+              merge.autoStash = true;
+              status.showStash = true;
+              push.autoSetupRemote = true;
+            };
+            aliases = {
+              tree = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
+            };
+          };
           signing = {
             key = "/home/${username}/.ssh/yubikey.pub";
             signByDefault = true;
             format = "ssh";
-          };
-          extraConfig = {
-            pull.rebase = true;
-            safe.directory = [
-              "/etc/nixos/.git"
-              "/etc/nixos"
-            ];
-            merge.conflictstyle = "zdiff3";
-            merge.tool = "vimdiff";
-            core.pager = "delta";
-            interactive.diffFilter = "delta --color-only";
-            delta = {
-              navigate = true;
-              dark = true;
-              side-by-side = true;
-            };
-            diff.colorMoved = "default";
-            rebase.autoStash = true;
-            merge.autoStash = true;
-            status.showStash = true;
-            push.autoSetupRemote = true;
-          };
-          aliases = {
-            tree = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
           };
         };
         gh = {
