2e98f3b6c6f97e47f236c5d74c0e20e4312b828c
eymeric <eymericdechelette@gmail.com>
2025-10-21 14:39:30 +0200

    feat: add second yubikey as root auth

--- apps/yubikey.nix
+++ apps/yubikey.nix
@@ -30,6 +30,7 @@ in
         authfile = pkgs.writeText "u2f" ''
           # 13/10/2025
           root:W9CHKqgATqKYaenRfuNjkc1cAXRjoshq3bpcF0vcOBh4s/MeoBodACyBo9qUoJHvkt8n7W7uJ2KfD/p9Q5xukw==,Hx2Jld8EMTl8C9eT+oIg6lzCMnTl2QF4PdlhFtdZuukuV3/hDlHf7Ltwo3/h+Go7X+xdgIb6AHCv7z2Enj6Bag==,es256,+presence
+          root:IGjPOv3ojp/lcJbrgX5uhUc7NlZLomMlXdFnsOnQaJDTVAzAEtmlCZzf+rnC9yUvHhMbKMjJKdQFXFDxnmStZg==,4an7GeeOLfKFyIlLO2iP1h/0vabU466rQ6EIs+Ms5fj9cU5tvnKRMET+uoVO+55gpVYXIfDRBh89CgH0Omtk1w==,es256,+presence
         '';
       };
     };
