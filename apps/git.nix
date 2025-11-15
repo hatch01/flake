c649d7354e4f9b69a4ca1d992c1c1c86543437a1
eymeric <hatchchien@protonmail.com>
2025-11-15 16:33:53 +0100

    Rename Git aliases to alias

--- apps/git.nix
+++ apps/git.nix
@@ -50,7 +50,7 @@ in
             merge.autoStash = true;
             status.showStash = true;
             push.autoSetupRemote = true;
-            aliases = {
+            alias = {
               tree = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
             };
           };
