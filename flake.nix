4bdd5d73b06adc38b2a02d2f1ec65859254e81f4
eymeric <hatchchien@protonmail.com>
2025-11-19 13:48:50 +0100

    feat: use new cuda cache server

--- flake.nix
+++ flake.nix
@@ -9,12 +9,14 @@
     substituters = [
       "https://cache.onyx.ovh"
       "https://cache.nixos.org"
+      "https://cache.nixos-cuda.org"
       "https://nix-community.cachix.org"
       "https://cache.flox.dev"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cache.onyx.ovh:2wUG6wsx5slbKUgkHT6GJuQ5k2StuUc8ysZQ2W+fbxA="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
     ];
