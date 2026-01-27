9a93754369f19c0e663b3984984335d91d03b135
eymeric <hatchchien@protonmail.com>
2026-01-27 19:45:28 +0100

    feat: reenable bitcoin

--- systems/jonquille/default.nix
+++ systems/jonquille/default.nix
@@ -39,7 +39,7 @@
   sslh.enable = true;
   wakapi.enable = true;
   vaultwarden.enable = true;
-  # bitcoin.server.enable = true;
+  bitcoin.server.enable = true;
 
   boot.loader.timeout = 1;
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
