216d663ec8ead11daa52a546f3c08906461a3516
eymeric <hatchchien@protonmail.com>
2026-03-01 11:18:15 +0100

    fix: remove useless patches

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -24,21 +24,6 @@
       hash = "sha256-zFEes9QKD5dTP/wa5ovtQRRYERtX1s4Ed3TRosevYoY=";
     }
     {
-      pr = 493404;
-      name = "python313Packages.sphinx-prompt: fix build";
-      hash = "sha256-m/Qp5Z7Cg/2v1W8maP6Bft/cCB3+ymfUUnqA3RekVa8=";
-    }
-    {
-      pr = 493406;
-      name = "python313Packages.sphinx-tabs: 3.4.7 -> 3.4.7-unstable-2026-01-24";
-      hash = "sha256-8pzmRnEmWokanWHKrpvIaKbxjQ3+Tld/4RiEdXD08uo=";
-    }
-    {
-      pr = 493700;
-      name = "utsushi: remove boost_system from autoconfig";
-      hash = "sha256-n5Bq8wUf3pQe0vT681rfaP6jBhsltl6Bc/o6uCef9cg=";
-    }
-    {
       pr = 493933;
       name = "nextcloudPackages: update";
       hash = "sha256-SoPsESUudB6K/B2g7+5t1lHE2mLVx7fa3EafGJPv3/k=";
@@ -46,11 +31,6 @@
   ];
 
   common = [
-    {
-      pr = 467484;
-      name = "sparrow";
-      hash = "sha256-r3tmo6i9Fqrx2LXySniNINtU6jNBjZZeacU5nr+Ir/U=";
-    }
     {
       pr = 402608;
       name = "satochip-utils";
