502954d9e124e0b3e7841f89d19a9001e2b32b06
eymeric <hatchchien@protonmail.com>
2026-03-02 21:18:30 +0100

    fix: remove useless patches

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -13,21 +13,6 @@
       name = "openthread-border-router";
       hash = "sha256-77CKiXwLAK4hgXPMN1lxYcLiWk44Rvs0Tion+t9fB6o=";
     }
-    {
-      pr = 461327;
-      name = "beszel-systemd";
-      hash = "sha256-O4xYtyyrSD/tQACSHebVnqVII6QzDAxH8SNIgo5e+7Y=";
-    }
-    {
-      pr = 490123;
-      name = "nvidia-fix";
-      hash = "sha256-zFEes9QKD5dTP/wa5ovtQRRYERtX1s4Ed3TRosevYoY=";
-    }
-    {
-      pr = 493933;
-      name = "nextcloudPackages: update";
-      hash = "sha256-SoPsESUudB6K/B2g7+5t1lHE2mLVx7fa3EafGJPv3/k=";
-    }
   ];
 
   common = [
