3a84e7db5d34296eac9821f1399b7a865dab0117
eymeric <hatchchien@protonmail.com>
2026-04-16 23:01:50 +0200

    prepare merge of 502388

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -3,11 +3,6 @@
 
   unstable = [
     {
-      pr = 502388;
-      name = "openthread-border-router";
-      hash = "sha256-AYx7ZSgj3v23qwdL6ItmlKeKh9i6uXyP+4NflS82r08=";
-    }
-    {
       pr = 499297;
       name = "hatchupdate";
       hash = "sha256-txQoGsOzimVsBlpJHo6xM6cttgSuQj34V/q/7u80svs=";
