7e548cfb978571a198e22a803dc655aead65c56e
eymeric <hatchchien@protonmail.com>
2026-06-03 17:32:34 +0200

    fix: remove old patches

--- patches.nix
+++ patches.nix
@@ -9,14 +9,6 @@
       name = "mautrix-telegram";
     }
     {
-      pr = 522784;
-      name = "termite: remove broken package with no live upstream";
-    }
-    {
-      pr = 523363;
-      name = "fix ecdsa";
-    }
-    {
       pr = 522225;
       name = "open-stage-control-headless: init at 1.30.3";
     }
