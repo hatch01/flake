fd7f13018c28e7c037d12ffb444427e7e4f92ca1
Forgejo Actions <actions@forge.onyx.ovh>
2026-07-27 06:25:29 +0200

    Format Nix code

--- systems/lilas/default.nix
+++ systems/lilas/default.nix
@@ -81,5 +81,6 @@
     crossPkgs.linuxPackagesFor kernelWithCcache
   );
 
-  services.beszel.agent.environment.EXTRA_FILESYSTEMS = "/var/lib/kvmd/msd__msd,/var/lib/kvmd/pst__pst";
+  services.beszel.agent.environment.EXTRA_FILESYSTEMS =
+    "/var/lib/kvmd/msd__msd,/var/lib/kvmd/pst__pst";
 }
