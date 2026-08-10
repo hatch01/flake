67a57f8e17c54ff953d53b80ae6508fab5104e58
System administrator <root@jonquille.jonquille>
2026-08-10 13:13:01 +0200

    feat: mautrix backfill

--- apps/matrix/discord.nix
+++ apps/matrix/discord.nix
@@ -28,6 +28,21 @@ in
       registerToSynapse = true;
       settings = {
         bridge = {
+          backfill = {
+            forward_limits = {
+              initial = {
+                dm = 100000;
+                channel = 100000;
+                thread = 100000;
+              };
+              missed = {
+                dm = 100000;
+                channel = 100000;
+                thread = 100000;
+              };
+            };
+            max_guild_members = -1;
+          };
           displayname_template = "{{ or .GlobalName .Username .ID }} (Discord)";
           permissions = {
             "*" = "relay";
