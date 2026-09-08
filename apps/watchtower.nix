a6d2b918e111d3a3ad5cca484f026e330ee57236
eymeric <hatchchien@protonmail.com>
2026-09-08 14:40:53 +0700

    feat: migrate to podman auto update

--- apps/watchtower.nix
+++ apps/watchtower.nix
@@ -12,12 +12,14 @@ in
   };
 
   config = mkIf config.watchtower.enable {
-    virtualisation.oci-containers.containers."watchtower" = {
-      autoStart = true;
-      image = "docker.io/nickfedor/watchtower";
-      volumes = [
-        "/var/run/docker.sock:/var/run/docker.sock"
-      ];
-    };
+    hmr.services.podman.enable = true;
+    hmr.services.podman.autoUpdate.enable = true;
+    # virtualisation.oci-containers.containers."watchtower" = {
+    #   autoStart = true;
+    #   image = "docker.io/nickfedor/watchtower";
+    #   volumes = [
+    #     "/var/run/docker.sock:/var/run/docker.sock"
+    #   ];
+    # };
   };
 }
