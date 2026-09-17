af9014b4db906323a7d6113e2d5ca15d383b05eb
eymeric <hatchchien@protonmail.com>
2026-09-17 12:36:02 +0200

    feat: docker tulipe

--- systems/tulipe/default.nix
+++ systems/tulipe/default.nix
@@ -38,6 +38,7 @@
   services.pulseaudio.enable = false;
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  container.docker.enable = true;
 
   # power management
   powerManagement.enable = true;
