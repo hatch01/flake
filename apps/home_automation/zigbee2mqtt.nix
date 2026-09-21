e604a218ff27569db026a2b910cfb26b452ebd20
eymeric <hatchchien@protonmail.com>
2026-09-21 22:46:13 +0200

    Revert "feat: z2m converter for presence sensor"
    This reverts commit 0bd59a6ebbeb70de12327dbc7a7b1608d0efcd51.

--- apps/home_automation/zigbee2mqtt.nix
+++ apps/home_automation/zigbee2mqtt.nix
@@ -1,7 +1,6 @@
 {
   lib,
   config,
-  pkgs,
   base_domain_name,
   ...
 }:
@@ -42,13 +41,6 @@ in
 
       settings = {
         version = 4;
-        external_converters = [
-          (pkgs.fetchurl {
-            name = "shs01_enhanced.js";
-            url = "https://raw.githubusercontent.com/notownblues/SHS-Z2M-Presence/main/zigbee2mqtt/external_converters/shs01_enhanced.js";
-            hash = "sha256-gFTLBt4AMBHHaHwcSbaPx8mLP1nRYtfwfmRLwHzFTo4=";
-          })
-        ];
         homeassistant.enabled = true;
         mqtt = {
           base_topic = "zigbee2mqtt";
